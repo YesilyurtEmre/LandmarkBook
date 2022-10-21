948a00c0692b8c0e147e2c52d8dbba4ed4d635c5
Emre <Yesilyurtemre@outlook.com.tr>
2022-10-21 13:42:29 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Emre Yeşilyurt on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
